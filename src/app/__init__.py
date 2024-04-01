09d3066c837306bbf1be1d5cf6ae7cc19ca3ada6
Jack Ablett <126977575+alycdev@users.noreply.github.com>
2024-04-01 21:09:10 +0100

    Added: Update notifier

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,16 @@
 from app.views import views
 from flask import Flask
+import requests
 
 app = Flask(__name__, static_folder="public")
 
-app.register_blueprint(views)
+with open("version") as version:
+    version = version.read()
+
+web_version = requests.get("https://raw.githubusercontent.com/MediahTM/Mediah-Local/main/src/version").text
+
+if float(version) >= float(web_version):
+    app.register_blueprint(views)
+else:
+    print("Please update your version of Mediah-Local\nVisit 'https://github.com/MediahTM/Mediah-Local' and follow the install instructions")
+    exit()
