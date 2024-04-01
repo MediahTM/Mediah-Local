1c16463b9b4090e759e6ed6aa423249c0b938515
Jack Ablett <126977575+alycdev@users.noreply.github.com>
2024-04-01 23:35:43 +0100

    Added: Search functionality

--- src/app/views.py
+++ src/app/views.py
@@ -4,8 +4,12 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("pages/index.html")
 
 @views.route("/view")
 def view():
-    return render_template("view.html")
+    return render_template("pages/view.html")
+
+@views.route("/search")
+def search():
+    return render_template("pages/search.html")
