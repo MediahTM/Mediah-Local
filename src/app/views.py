f42957c0727fe1cbce33375d24ed99189f603813
Jack Ablett <126977575+alycdev@users.noreply.github.com>
2024-04-02 11:56:30 +0100

    Updated TMDB interaction

--- src/app/views.py
+++ src/app/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template, request
 
 views = Blueprint("views", __name__)
 
+@views.context_processor
+def globals():
+    return {
+        "tmdb_apikey": "bfe60268dcb9f6352a6ff6ce40312265" # This can be left public!
+    }
+
 @views.route("/")
 def index():
     return render_template("pages/index.html")
